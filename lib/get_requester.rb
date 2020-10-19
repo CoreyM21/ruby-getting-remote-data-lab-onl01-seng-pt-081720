a8580e868f7f89bea7626af3e61460e5d2aaedb0
Corey Martin <coreyjmartin2@gmail.com>
2020-10-19 19:21:30 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -17,7 +17,7 @@ class GetRequester
   end
     
   def parse_json
-        response_body = JSON.parse(self.get_response_body)
-   end
+    response_body = JSON.parse(self.get_response_body)
+  end
   
 end
